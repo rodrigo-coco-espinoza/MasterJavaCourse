9335a3981cd9359cee8ab64cf3f4b2e63cce2053
rodrigo-coco-espinoza <respinozafuentes@gmail.com>
2025-05-07 17:32:59 -0400

    Refactor quiz functionality to implement playQuiz method and enhance user input handling

--- QuizConsoleApp/Main.java
+++ QuizConsoleApp/Main.java
@@ -4,7 +4,8 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("Welcome to the Quiz Console App!");
+        System.out.println();
         QuestionService questionService = new QuestionService();
-        questionService.displayQuestions();
+        questionService.playQuiz();
     }
 }
--- QuizConsoleApp/QuestionService.java
+++ QuizConsoleApp/QuestionService.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class QuestionService 
 {
-    Question [] questions = new Question[5];
+    Question questions[] = new Question[5];
+    int answers[] = new int[5];
 
     public QuestionService()
     {
@@ -56,16 +59,63 @@ public class QuestionService
         );
     }
 
-    public void displayQuestions()
+    public void displayQuestion(Question question)
+    {       
+        System.out.println("Question " + question.getId() + ": " + question.getQuestionText());
+        for (int i = 0; i < question.getOptions().length; i++)
+        {
+            System.out.println((i + 1) + ". " + question.getOptions()[i].getText());
+        }        
+    }
+
+    public int getAnswer(Question question)
     {
+        boolean validInput = false;
+        int answer;
+        System.out.print("Enter your answer (1-" + question.getOptions().length + "): ");
+        do
+        {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            try
+            {
+                answer = Integer.parseInt(input);
+                if (answer >= 1 && answer <= question.getOptions().length)
+                {
+                    validInput = true;
+                    System.out.println();
+                    return answer - 1; // Adjust for zero-based index
+                }
+                else
+                {
+                    System.out.print("Invalid input. Please enter a number between 1 and " + question.getOptions().length + ": ");
+                }
+            }
+            catch (NumberFormatException e)
+            {
+                System.out.print("Invalid input. Please enter a number between 1 and " + question.getOptions().length + ": ");
+            }
+        }
+        while (!validInput);
+
+        return -1; // This line will never be reached due to the loop condition
+    }
+
+    public void playQuiz()
+    {
+        int answerIndex = 0;
         for (Question question : questions)
         {
-            System.out.println("Question " + question.getId() + ": " + question.getQuestionText());
-            for (int i = 0; i < question.getOptions().length; i++)
-            {
-                System.out.println((i + 1) + ". " + question.getOptions()[i].getText());
-            }
-            System.out.println();
+            displayQuestion(question);
+            // Get user input for the answer
+            answers[answerIndex] = getAnswer(question);
+            answerIndex++;
+        }
+        
+        System.out.println("Your answers are:");
+        for (int i = 0; i < answers.length; i++)
+        {
+            System.out.println("Question " + (i + 1) + ": " + questions[i].getOptions()[answers[i]].getText());
         }
     }
 }
