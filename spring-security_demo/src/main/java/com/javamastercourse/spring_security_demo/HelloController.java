83d438a0d3d2095479fb71184c5f6d81fc1d0f6c
rodrigo-coco-espinoza <respinozafuentes@gmail.com>
2025-06-06 15:07:54 -0400

    feat: Add Student model and controller; implement student management endpoints and integrate Lombok for boilerplate reduction

--- spring-security_demo/src/main/java/com/javamastercourse/spring_security_demo/HelloController.java
+++ spring-security_demo/src/main/java/com/javamastercourse/spring_security_demo/HelloController.java
@@ -3,12 +3,21 @@ package com.javamastercourse.spring_security_demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 public class HelloController 
 {
     @GetMapping("/greet")
-    public String greet()
+    public String greet(HttpServletRequest request)
     {
-        return "Hello, World!";
+        return "Hello, World!" + request.getSession().getId();
+    }
+
+
+    @GetMapping("/about")
+    public String about(HttpServletRequest request)
+    {
+        return "This is a Spring Security demo application." + request.getSession().getId();
     }
 }
