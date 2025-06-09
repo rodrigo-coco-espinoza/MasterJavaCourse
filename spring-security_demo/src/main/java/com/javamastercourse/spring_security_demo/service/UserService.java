d2830cb40bbd0fc5004acbfb769a3429657d17d2
Coco <respinozafuentes@gmail.com>
2025-06-09 19:06:31 -0400

    feat: Update password encoding to use BCryptPasswordEncoder for enhanced security

--- spring-security_demo/src/main/java/com/javamastercourse/spring_security_demo/service/UserService.java
+++ spring-security_demo/src/main/java/com/javamastercourse/spring_security_demo/service/UserService.java
@@ -1,6 +1,7 @@
 package com.javamastercourse.spring_security_demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.javamastercourse.spring_security_demo.dao.UserRepository;
@@ -9,11 +10,14 @@ import com.javamastercourse.spring_security_demo.model.User;
 @Service
 public class UserService {
 
+    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(12);
+
     @Autowired
     private UserRepository repository;
 
     public User saveUser(User user) 
     {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return repository.save(user);
     }
 }
